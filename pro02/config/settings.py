62725d3fae745b1088cb58b1a5037b0742c19f97
xddongx <31644115+xddongx@users.noreply.github.com>
2021-02-08 10:07:14 +0900

    Update settings.py

--- pro02/config/settings.py
+++ pro02/config/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import conn_Mysql, os
+# import conn_Mysql, os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,17 +76,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': conn_Mysql.NAME,
+#         'USER': conn_Mysql.USER,
+#         'PASSWORD': conn_Mysql.PASSWORD,
+#         'HOST': conn_Mysql.HOST,
+#         'PORT': conn_Mysql.PORT,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': conn_Mysql.NAME,
-        'USER': conn_Mysql.USER,
-        'PASSWORD': conn_Mysql.PASSWORD,
-        'HOST': conn_Mysql.HOST,
-        'PORT': conn_Mysql.PORT,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
