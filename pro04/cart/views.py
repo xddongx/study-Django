7b008919e9f491c6108a11dca2ead4cadc8740f1
xddongx <31644115+xddongx@users.noreply.github.com>
2021-02-09 16:59:13 +0900

    cart

--- pro04/cart/views.py
+++ pro04/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from ..shop.models import Product
+from shop.models import Product
 from .forms import AddProductForm
 from .cart import Cart
 @require_POST
