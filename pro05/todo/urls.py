013b3ff492670017704afcbd6d9708d2d749372f
xddongx <31644115+xddongx@users.noreply.github.com>
2021-02-15 15:29:27 +0900

    vue.js .....?

--- pro05/todo/urls.py
+++ pro05/todo/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo.urls')),
+    path('', index, name='index'),
+    path('fetch/', todo_fetch, name='fetch'),
+    path('save/', todo_save, name='save'),
 ]
